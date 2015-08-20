a72c266e187b6b08da90dba27edb51b2bdcb31c3
Bastien Ledon <bastien.ledon@yahoo.com>
2015-08-20 22:39:28 +0200

    message log

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express'),
     app = express(),
     http = require('http').Server(app),
-    Application = require('./src/Application.js');
+    Application = require('./src/Application.js'),
+    logger = require('./src/Logger.js');
 
 app.io = require('socket.io')(http);
 
@@ -12,5 +13,5 @@ app.disable('x-powered-by');
 Application = new Application(app).launch();
 
 http.listen(42594, function () {
-
+    logger.log('info', 'Web Server started, on port 42594');
 });
